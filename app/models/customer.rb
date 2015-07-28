b03e520b2330fdafc35f099dbe8f7e4f83f8d5e0
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-28 11:50:28 -0600

    Add few more relationships between models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
-  has_many :merchants, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions,  through: :invoices
+  has_many :merchants,     through: :invoices
 
   def search
     @customers = Customer.all
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,14 @@
 class Merchant < ActiveRecord::Base
-  has_many :invoices
   has_many :items
-  has_many :customers, through: :invoices
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :customers,     through: :invoices
+  has_many :transactions,  through: :invoices
+
+
+  def revenue
+    Merchant.invoice_item.reduce(0) do |sum, inv_item|
+      (sum + (inv_item.unit_price / 100.00) * inv_item.quantity)
+    end
+  end
 end
